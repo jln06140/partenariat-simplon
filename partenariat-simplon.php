53324feb78bb86245a71ec96fdecccaf8ee35cd6
jln06140 <32737228+jln06140@users.noreply.github.com>
2017-12-07 17:46:26 +0100

    ajout plugin simplon

--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,10 +1,14 @@
 <?php
 /*
-
+Plugin Name: simplon_plugin
+Author: Jean-Luc
 */
 
-
-
-
+function ShortcodeSimplon() {
+	return '<p>La publication de cet article est possible grâce à mon super partenaire simplon.co - une entreprise de
+    l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+    des formations GRATUITES pour devenir développeur web.</p>';
+}
+add_shortcode('simplon', 'ShortcodeSimplon');
 
 ?>
